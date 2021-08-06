022533e99d82766e98f9bd20686e81303bfd0d73
Leonardo Martins <leonardomartins07@gmail.com>
2021-08-05 21:43:16 -0300

    MovieLibrary

---
+++ README.md
@@ -0,0 +1,4 @@
+### Projeto incompleto
+## Sim, eu sei que não está nem um pouco apresentavel.
+
+Estou com dificuldade em pensar no css mas ele está funcionando, então é isso, já já arrumo o css. (-_-')
---
+++ src/pages/MovieList.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Loading } from '../components';
+import MovieCard from '../components/MovieCard';
+
+import * as movieAPI from '../services/movieAPI';
+
+class MovieList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: [],
+      isLoading: true,
+    };
+
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies() {
+    movieAPI.getMovies()
+      .then((movie) => this.setState({ movies: movie, isLoading: false }));
+  }
+
+  render() {
+    const { movies, isLoading } = this.state;
+    return (
+      <div data-testid="movie-list">
+        {isLoading && <Loading />}
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
